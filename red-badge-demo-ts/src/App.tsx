a634469937c8dc10e66278c585cae3e3bf5f31c4
jesnadolski <nadolski.jessica@gmail.com>
2021-03-23 19:47:24 -0400

    basic react classes

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import PropsExample from './components/PropsExample';
+import PropsMapping from './components/PropsMapping';
 
 function App() {
-  return <div></div>;
+const visitedPlaces = ['Italy', 'Germany', 'Spain', 'South Africa', 'Egypt', 'South Korea', 'Ireland', 'Morocco', 'Andora', 'France', 'Colombia', 'Jamaica', 'Mexico', 'Aruba','Bahamas','Cayman Islands', 'Austria', 'Netherlands', 'Poland', 'Vietnam', 'Thailand', 'Greece', 'Austria'];
+
+  return (
+    <div>
+ <div><PropsExample name='Tom' business='MySpace' /></div>
+ <div><PropsMapping visited={visitedPlaces}/></div>
+ </div>
+  );
 }
 
 export default App;
