dd6a7f71ad24df02a53181ddcb2be6b418c92e3d
jesnadolski <nadolski.jessica@gmail.com>
2021-03-25 19:31:16 -0400

    class challenge

--- red-badge-demo-ts/src/components/ClassClickCounter.jsx
+++ red-badge-demo-ts/src/components/ClassClickCounter.jsx
@@ -1,1 +1,27 @@
 // Create a class version of the Functional Component
+
+import React, { Component } from 'react';
+import {Container, Button} from 'reactstrap';
+
+class ClassClickCounter extends Component {
+constructor(props){
+super(props);
+this.state = {countValue: this.props.initialValue};
+
+}
+
+handleClick = () => {
+    this.setState({countValue: this.state.countValue - 1});
+}
+
+    render(){
+        return(
+            <Container>
+            <p>The total clicks left are {this.state.countValue}</p>
+            <Button onClick={this.handleClick}></Button>
+            </Container>
+        )
+    }
+}
+
+export default ClassClickCounter;
